eaceb5ae3a885afc7294cc7768fccbae3dbece56
Camila <97048604+camibarbosa@users.noreply.github.com>
2024-02-26 20:18:54 -0300

    feat: added route path

--- crud-angular/src/app/app.routes.ts
+++ crud-angular/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'products' },
+  { path: 'products',
+  loadChildren: () => import('./products/products.module').then(m => m.ProductsModule) }
+];
